eb32c816bf98e731357b79cb51ae85a79b8c56e0
Gopinathan <gopinathan.anandan@gmail.com>
2024-05-21 06:37:26 +0530

    changes

--- Q1/js/script.js
+++ Q1/js/script.js
@@ -1,7 +1,7 @@
 let obj1 = {name: "Person 1", age: 5};
 let obj2 = {age: 5, name: "Person 1"};
 
-function compareObjects(a1, a2) {
+function compare(a1, a2) {
    
     const str1 = JSON.stringify(a1, Object.keys(a1).sort());
     const str2 = JSON.stringify(a2, Object.keys(a2).sort());
@@ -9,4 +9,4 @@ function compareObjects(a1, a2) {
     return str1 === str2;
 }
 
-console.log(compareObjects(obj1, obj2)); 
+console.log(compare(obj1, obj2)); 
