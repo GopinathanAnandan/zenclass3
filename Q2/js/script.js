e2b6a306ed02747a3f8135ecd0208a276add957b
Gopinathan <gopinathan.anandan@gmail.com>
2024-05-21 06:44:41 +0530

    Q2-Created

---
+++ Q2/js/script.js
@@ -0,0 +1,15 @@
+function reqListener() {
+    const countryarray = JSON.parse(this.responseText);
+    console.log(countryarray);
+    for(let country of countryarray){
+        console.table(country.flags.png);
+        
+    }
+
+  }
+  
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", reqListener);
+  req.open("GET", "https://restcountries.com/v3.1/all");
+  req.send();
+  
